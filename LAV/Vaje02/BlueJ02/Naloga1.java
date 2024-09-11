d13795ddef0d26a59817712c28af981b7ef25cef
lin-cadez <lin.cadez@gmail.com>
2024-09-11 14:52:31 +0200

    VAJE02

---
+++ LAV/Vaje02/BlueJ02/Naloga1.java
@@ -0,0 +1,7 @@
+public class Naloga1 {
+    
+
+  public static void main(String ime, String priimek){
+    System.out.println(ime.substring(0, 1) + " " + priimek.charAt(0));
+   }
+}
---
+++ LAV/Vaje02/BlueJ02/Naloga3.java
@@ -0,0 +1,8 @@
+public class Naloga3 {
+  public static void main(int a, int b){
+      
+    System.out.println("vsota: " + (a+b));
+    System.out.println("razlika: " + (a-b));
+    System.out.println("produkt: " + (a*b));
+    System.out.println("količnik: " + (a/b));}
+}
